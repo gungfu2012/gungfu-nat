e1b9abba2a254319b67748b6a497d949e00b34dd
gungfu2012 <gungfu2012@hotmail.com>
2021-01-29 16:40:19 +0800

    create client and localserver

---
+++ client/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net"
+	"net/http"
+	"strconv"
+)
+
+var remoteserver string = "ws://192.168.1.168:8080/"
+
+const bufmax uint = 1 << 20
+
+func readfromconn(conn net.Conn, wsconn *websocket.Conn) {
+	var buf [bufmax]byte
+	for {
+		if conn == nil || wsconn == nil {
+			break
+		}
+		n, err := conn.Read(buf[0:bufmax])
+		if err != nil && n == 0 {
+			conn.Close()
+			break
+		}
+		if n == 0 {
+			continue
+		}
+		err = wsconn.WriteMessage(websocket.BinaryMessage, buf[0:n])
+		if err != nil {
+			wsconn.Close()
+			break
+		}
+	}
+}
+
+func writetoconn(conn net.Conn, wsconn *websocket.Conn) {
+	for {
+		if conn == nil || wsconn == nil {
+			break
+		}
+		_, buf, err := wsconn.ReadMessage()
+		if err != nil {
+			wsconn.Close()
+			break
+		}
+		_, err = conn.Write(buf)
+		if err != nil {
+			conn.Close()
+			break
+		}
+	}
+}
+func main() {
+	var port, path string
+	flag.StringVar(&port, "port", "2222", "default port for ssh")
+	flag.StringVar(&path, "path", "ssh_client", "default path for ssh")
+	flag.Parse()
+
+	var index int = 0
+	var header = http.Header{}
+	header.Add("conn-index", strconv.Itoa(index))
+
+	ln, err := net.Listen("tcp", ":"+port)
+	fmt.Println(err)
+	fmt.Println("start listen the ", port, " port")
+	for {
+		conn, _ := ln.Accept()
+		wsconn, _, _ := websocket.DefaultDialer.Dial(remoteserver+path, header)
+		go readfromconn(conn, wsconn)
+		go writetoconn(conn, wsconn)
+
+		index = (index + 1) % 65536
+		header.Set("conn-index", strconv.Itoa(index))
+	}
+}
---
+++ localserver/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"net"
+	"net/http"
+	"strconv"
+)
+
+var remoteserver string = "ws://127.0.0.1:8080/"
+
+const ctlpath string = "control"
+const bufmax uint = 1 << 20
+
+func readfromconn(conn net.Conn, wsconn *websocket.Conn) {
+	var buf [bufmax]byte
+	for {
+		if conn == nil || wsconn == nil {
+			break
+		}
+		n, err := conn.Read(buf[0:bufmax])
+		if err != nil && n == 0 {
+			conn.Close()
+			break
+		}
+		if n == 0 {
+			continue
+		}
+		err = wsconn.WriteMessage(websocket.BinaryMessage, buf[0:n])
+		if err != nil {
+			wsconn.Close()
+			break
+		}
+	}
+}
+
+func writetoconn(conn net.Conn, wsconn *websocket.Conn) {
+	for {
+		if conn == nil || wsconn == nil {
+			break
+		}
+		_, buf, err := wsconn.ReadMessage()
+		if err != nil {
+			wsconn.Close()
+			break
+		}
+		_, err = conn.Write(buf)
+		if err != nil {
+			conn.Close()
+			break
+		}
+	}
+}
+func main() {
+	var port, path string
+	//flag.StringVar(&port, "port", "2222", "default port for ssh")
+	//flag.StringVar(&path, "path", "ssh_client", "default path for ssh")
+	flag.Parse()
+
+	var index int = 0
+	var header = http.Header{}
+	header.Add("conn-index", strconv.Itoa(index))
+
+	ctlconn, _, _ := websocket.DefaultDialer.Dial(remoteserver+ctlpath, nil)
+	fmt.Println("create ctl connection")
+
+	for {
+		mt, buf, _ := ctlconn.ReadMessage()
+		if mt != websocket.BinaryMessage {
+			continue
+		}
+		switch buf[0] {
+		case 0: //ssh
+			path = "ssh_localserver"
+			port = "22"
+		case 1: //emby
+			path = "emby_localserver"
+			port = "8096"
+		}
+		header.Set("conn-index", string(buf[2:2+buf[1]]))
+		wsconn, _, _ := websocket.DefaultDialer.Dial(remoteserver+path, header)
+		conn, _ := net.Dial("tcp", "127.0.0.1:"+port)
+		go readfromconn(conn, wsconn)
+		go writetoconn(conn, wsconn)
+	}
+}
